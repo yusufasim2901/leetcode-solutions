f575321a5652549c9dde0e6672f83ffc128b86d0
yusufasim <erenyusufasim@gmail.com>
2025-04-17 00:05:08 +0300

    solution added

---
+++ easy/S383.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            letters[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (--letters[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
