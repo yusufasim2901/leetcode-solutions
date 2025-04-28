658d5147e4b235cee699eb7821441ac307f3b561
yusufasim <erenyusufasim@gmail.com>
2025-04-28 16:19:28 +0300

    solution added

---
+++ easy/S392.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        int n = s.length(), m = t.length();
+        
+        while (i < n && j < m) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        
+        
+        return i == n;
+    }
+}
