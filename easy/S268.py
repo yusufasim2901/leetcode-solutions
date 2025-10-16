795c8e996590a77d4817c32ea33d6eaf75d1c5ac
yusufasim <erenyusufasim@gmail.com>
2025-10-16 15:46:53 +0300

    solution added

---
+++ easy/S268.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if i != nums[i]:    
+                return i
+        return len(nums)    
