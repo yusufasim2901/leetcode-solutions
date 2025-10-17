810a13190322c22ab0b9585b65c038ebaca540d5
yusufasim <erenyusufasim@gmail.com>
2025-10-17 23:21:10 +0300

    solution added

---
+++ medium/S204.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
+        
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False 
+        
+        
+        for p in range(2, int(n**0.5) + 1):
+            if is_prime[p]:
+                for multiple in range(p * p, n, p):
+                    is_prime[multiple] = False
+                    
+        
+        return sum(is_prime)
