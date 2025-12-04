7d0cda0fb712bd0e98aadae0aaee6de1006fa022
yusufasim <erenyusufasim@gmail.com>
2025-12-04 13:42:03 +0300

    solution added

---
+++ medium/S46.py
@@ -0,0 +1,6 @@
+from itertools import permutations
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return [list(i) for i in permutations(nums)]
