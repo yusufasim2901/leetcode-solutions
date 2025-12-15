1d9873f7ca9984a0364e0f4cdc146f5d82b8cd4f
yusufasim <erenyusufasim@gmail.com>
2025-12-15 12:47:53 +0300

    solution added

---
+++ easy/S350.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+
+        return list((c1 & c2).elements())
